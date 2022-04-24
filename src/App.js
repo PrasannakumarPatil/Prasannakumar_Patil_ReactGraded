6e481b255ee300fdb322ade68c111ef41e1b182a
Prasannakumar <prasannacpatil@gmail.com>
2022-04-24 19:27:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+import Homepage from './components/Homepage'
+function App() {
+  return (
+    <div
+      style={{
+        display: 'block',
+        position: 'absolute',
+        top: '0',
+        bottom: '0',
+        left: '0',
+        right: '0',
+      }}
+    >
+      <Homepage />
+    </div>
+  )
+}
+
+export default App
